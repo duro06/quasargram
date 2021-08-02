fa3c15c60b8bc42de7fef75b63bc9acca9a5e508
duro06 <andi.meka.025@gmail.com>
2021-08-02 12:42:33 +0700

    init repo but well i had do a lot

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,66 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-white text-grey-10">
+      <q-toolbar>
+        <q-btn
+          :to="{ name: 'camera' }"
+          class="large-screen-only"
+          icon="eva-camera-outline"
+          size="18px"
+          flat
+          round
+          dense
+        />
+        <q-toolbar-title class="text-grand-hotel text-bold">
+          Quasargram
+        </q-toolbar-title>
+        <q-btn
+          :to="{ name: 'home' }"
+          class="large-screen-only"
+          icon="eva-home-outline"
+          size="18px"
+          flat
+          round
+          dense
+        />
+
+        <!-- <div>Quasar v{{ $q.version }}</div> -->
+      </q-toolbar>
+    </q-header>
+
+    <q-footer class="bg-white small-screen-only" bordered>
+      <q-tabs
+        class="text-grey-10"
+        active-color="primary"
+        indicator-color="transparent"
+      >
+        <q-route-tab :to="{ name: 'home' }" icon="eva-home-outline" />
+        <q-route-tab :to="{ name: 'camera' }" icon="eva-camera-outline" />
+      </q-tabs>
+    </q-footer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  setup() {
+    return {};
+  },
+});
+</script>
+<style lang="sass">
+.q-toolbar__title
+  text-align: center
+  font-size: 30px
+.q-footer
+  .q-tab__icon
+    font-size: 30px
+</style>
---
+++ src/router/routes.js
@@ -0,0 +1,23 @@
+const routes = [
+  {
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      { path: "", name: "home", component: () => import("pages/PageHome.vue") },
+      {
+        path: "/camera",
+        name: "camera",
+        component: () => import("pages/PageCamera.vue"),
+      },
+    ],
+  },
+
+  // Always leave this as last one,
+  // but you can also remove it
+  {
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/Error404.vue"),
+  },
+];
+
+export default routes;
