9903a2655c173dca13c769e89b48119e2f3bd5ab
duro06 <andi.meka.025@gmail.com>
2021-08-06 07:29:36 +0700

    camera, location ok

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,13 +1,248 @@
 <template>
-  <q-page class="flex flex-center">
-    <h5>Camera Page</h5>
+  <q-page class="constrain-more q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <!-- <img class="full-width" src="https://cdn.quasar.dev/img/mountains.jpg" /> -->
+      <video v-show="!imageCaptured" ref="video" class="full-width" autoplay />
+      <canvas v-show="imageCaptured" ref="canvas" class="full-with" height="240" />
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn
+        v-if="hasCameraSupport"
+        @click="captureImage"
+        color="grey-10"
+        icon="eva-camera"
+        size="lg"
+        round
+      />
+      <q-file
+        v-if="!hasCameraSupport"
+        label="Choose n image"
+        outlined
+        accept="image/*"
+        v-model="imageUpload"
+        @input="captureImageFallback"
+      >
+        <template v-slot:prepend>
+          <q-icon name="eva-attach-outline" />
+        </template>
+      </q-file>
+    </div>
+    <div class="row justify-center q-pa-md">
+      <q-input v-model="caption" class="col col-sm-6" label="Caption" dense />
+    </div>
+    <div class="row justify-center q-pa-md">
+      <q-input
+        v-model="location"
+        class="col col-sm-6"
+        label="Location"
+        dense
+        :loading="locLoading"
+      >
+        <template v-slot:append>
+          <q-btn
+            v-if="!locLoading && locationSupported"
+            @click="getLocation"
+            round
+            dense
+            flat
+            icon="eva-navigation-2-outline"
+          />
+        </template>
+      </q-input>
+    </div>
+    <div class="row justify-center q-mt-lg">
+      <q-btn @click="postImage" color="primary" label="Post Image" rounded unelevated />
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from "vue";
-
+import { defineComponent, ref, computed, onMounted, onBeforeUnmount } from 'vue'
+import { uid, useQuasar } from 'quasar'
+import axios from 'axios'
+require('md-gum-polyfill')
 export default defineComponent({
-  name: "PageCamera",
-});
+  name: 'PageCamera',
+  setup() {
+    const $q = useQuasar()
+    let id = ref(uid())
+    let caption = ref('')
+    let location = ref('')
+    let photo = ref(null)
+    let date = ref(Date.now())
+    let video = ref(null)
+    let canvas = ref(null)
+    let imageCaptured = ref(false)
+    let hasCameraSupport = ref(true)
+    let imageUpload = ref(null)
+    let locLoading = ref(false)
+    let width = 0
+    let height = 0
+    let locationSupported = computed(() => {
+      if ('geolocation' in navigator) return true
+      return false
+    })
+    let initCamera = () => {
+      width = video.value.getBoundingClientRect().width
+      height = video.value.getBoundingClientRect().height
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((stream) => {
+          video.value.srcObject = stream
+          // console.log(video.value)
+        })
+        .catch(() => {
+          hasCameraSupport.value = false
+        })
+    }
+    let captureImage = () => {
+      canvas.value.width = video.value.getBoundingClientRect().width
+      canvas.value.height = video.value.getBoundingClientRect().height
+      let context = canvas.value.getContext('2d')
+      context.drawImage(video.value, 0, 0, canvas.value.width, canvas.value.height)
+      imageCaptured.value = true
+      photo.value = dataURItoBlob(canvas.value.toDataURL())
+      disableCamera()
+      // console.log('video', video.value)
+      console.log('photo', photo.value)
+    }
+    let captureImageFallback = (apem) => {
+      photo.value = apem
+      canvas.value.width = width
+      canvas.value.height = height
+      var reader = new FileReader()
+      let context = canvas.value.getContext('2d')
+      reader.onload = (event) => {
+        var img = new Image()
+        img.onload = () => {
+          // kecilin
+          // canvas.value.width = img.width * 0.5
+          // canvas.value.height = img.height * 0.5
+
+          context.drawImage(img, 0, 0, canvas.value.width, canvas.value.height)
+          imageCaptured.value = true
+        }
+        img.src = event.target.result
+      }
+      // reader.readAsDataURL(apem)
+      reader.readAsDataURL(apem.target.files[0])
+    }
+    let disableCamera = () => {
+      if (video.value.srcObject !== null) {
+        video.value.srcObject.getVideoTracks().forEach((track) => {
+          track.stop()
+        })
+      }
+    }
+    let postImage = () => {
+      let post = {
+        id: id.value,
+        caption: caption.value,
+        location: location.value,
+        photo: photo.value,
+        date: date.value,
+      }
+      console.log('post', post)
+    }
+    let getLocation = () => {
+      locLoading.value = true
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          getCityAndCountry(position)
+          // console.log('position', position)
+        },
+        (err) => {
+          errorDialog('get your Position')
+          locLoading.value = false
+        },
+        {
+          timeout: 7000,
+        }
+      )
+    }
+    let getCityAndCountry = (position) => {
+      let apiUrl = `https://geocode.xyz/${position.coords.latitude},${position.coords.longitude}?json=1`
+      axios
+        .get(apiUrl)
+        .then((resp) => {
+          // console.log(resp)
+          locationSuccess(resp)
+        })
+        .catch((err) => {
+          locLoading.value = false
+          errorDialog('get your City name')
+        })
+    }
+    let locationSuccess = (data) => {
+      locLoading.value = false
+      location.value = data.data.city
+      if (data.data.state) {
+        location.value += `, ${data.data.state}`
+      }
+      if (data.data.country) {
+        location.value += `, ${data.data.country} `
+      }
+    }
+    let errorDialog = (data) => {
+      $q.dialog({
+        title: 'Error',
+        message: `Cannot ${data} `,
+      })
+    }
+    onMounted(() => {
+      initCamera()
+      console.log(locationSupported.value)
+    })
+    onBeforeUnmount(() => {
+      if (hasCameraSupport) {
+        disableCamera()
+      }
+    })
+    function dataURItoBlob(dataURI) {
+      // convert base64 to raw binary data held in a string
+      // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+      var byteString = atob(dataURI.split(',')[1])
+
+      // separate out the mime component
+      var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+      // write the bytes of the string to an ArrayBuffer
+      var ab = new ArrayBuffer(byteString.length)
+
+      // create a view into the buffer
+      var ia = new Uint8Array(ab)
+
+      // set the bytes of the buffer to the correct values
+      for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i)
+      }
+
+      // write the ArrayBuffer to a blob, and you're done
+      var blob = new Blob([ab], { type: mimeString })
+      return blob
+    }
+
+    return {
+      caption,
+      location,
+      postImage,
+      video,
+      canvas,
+      photo,
+      imageCaptured,
+      hasCameraSupport,
+      locLoading,
+      locationSupported,
+      imageUpload,
+      captureImage,
+      captureImageFallback,
+      getLocation,
+    }
+  },
+})
 </script>
+<style lang="sass">
+.camera-frame
+  border: 2px solid $grey-10
+  border-radius: 10px
+</style>
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,32 +1,52 @@
 <template>
   <q-page class="constrain q-pa-md">
-    <q-card
-      v-for="(post, index) in posts"
-      :key="index"
-      class="card-post q-mb-md"
-      flat
-      bordered
-    >
-      <q-item>
-        <q-item-section avatar>
-          <q-avatar>
-            <img src="image/aku.jpg" />
-          </q-avatar>
-        </q-item-section>
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <q-card
+          v-for="(post, index) in posts"
+          :key="index"
+          class="card-post q-mb-md"
+          flat
+          bordered
+        >
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="image/aku.jpg" />
+              </q-avatar>
+            </q-item-section>
 
-        <q-item-section>
-          <q-item-label class="text-bold">Andi Dermawan</q-item-label>
-          <q-item-label caption> {{ post.location }} </q-item-label>
-        </q-item-section>
-      </q-item>
+            <q-item-section>
+              <q-item-label class="text-bold">Duro06</q-item-label>
+              <q-item-label caption> {{ post.location }} </q-item-label>
+            </q-item-section>
+          </q-item>
 
-      <q-separator />
-      <q-img :src="post.image" />
-      <q-card-section>
-        <div>{{ post.caption }}</div>
-        <div class="text-caption text-grey">{{ post.date }}</div>
-      </q-card-section>
-    </q-card>
+          <q-separator />
+          <q-img :src="post.image" />
+          <q-card-section>
+            <div>{{ post.caption }}</div>
+            <div class="text-caption text-grey">
+              {{ $filters.niceDate(post.date) }}
+            </div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-4 large-screen-only">
+        <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img src="image/aku.jpg" />
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label class="text-bold">Duro06</q-item-label>
+            <q-item-label caption> Andi Dermawan</q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -34,47 +54,47 @@
 const pots = [
   {
     id: 1,
-    caption: 'apem',
+    caption: "apem",
     date: 1627892570837,
-    location: 'Bucor Kulon',
-    image: 'https://cdn.quasar.dev/img/mountains.jpg',
+    location: "Bucor Kulon",
+    image: "https://cdn.quasar.dev/img/mountains.jpg",
   },
   {
     id: 2,
-    caption: 'apem',
+    caption: "apem",
     date: 1627892570837,
-    location: 'Bucor Kulon',
-    image: 'https://cdn.quasar.dev/img/mountains.jpg',
+    location: "Bucor Kulon",
+    image: "https://cdn.quasar.dev/img/mountains.jpg",
   },
   {
     id: 3,
-    caption: 'apem',
+    caption: "apem",
     date: 1627892570837,
-    location: 'Bucor Kulon',
-    image: 'https://cdn.quasar.dev/img/mountains.jpg',
+    location: "Bucor Kulon",
+    image: "https://cdn.quasar.dev/img/mountains.jpg",
   },
   {
     id: 4,
-    caption: 'apem',
+    caption: "apem",
     date: 1627892570837,
-    location: 'Bucor Kulon',
-    image: 'https://cdn.quasar.dev/img/mountains.jpg',
+    location: "Bucor Kulon",
+    image: "https://cdn.quasar.dev/img/mountains.jpg",
   },
-]
-import { defineComponent, reactive, toRefs, onMounted } from 'vue'
+];
+import { defineComponent, reactive, toRefs, onMounted } from "vue";
 
 export default defineComponent({
-  name: 'PageHome',
+  name: "PageHome",
   setup() {
     let state = reactive({
       posts: [],
-    })
+    });
     onMounted(() => {
-      state.posts = pots
-    })
-    return { ...toRefs(state) }
+      state.posts = pots;
+    });
+    return { ...toRefs(state) };
   },
-})
+});
 </script>
 <style lang="sass">
 .card-post
